77d86f964917f666d57565c1a0640d75d3be4058
Ryan Freese <65748652+KeepTheLidOnTight@users.noreply.github.com>
2020-10-01 21:28:53 -0500

    Update script.js
    Figuring out local storage and manipulation of styling through the JS file.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-// document.ready is going o render the the html and css before going into the function
+// document.ready is going to render the the html and css before going into the function
 $(document).ready(function () {
     
    
    
-    // trying to get the date to load in the jumbotron class of the header tag
-    $("#currentDay").text(moment().format("MMMM Do YYYY"));
+    // trying to get the date to load in the jumbotron class of the header tag//used momentjs
+    $("#currentDay").text(moment().format("MMM Do YY"));
 
     // creating a on click event for when the user presses the save button in each time block.
     $(".saveBtn").on("click", function () {
@@ -18,7 +18,7 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
     //figure out a way to load the local storage from each of the time blocks created (1 for every hour)
-    //24 clock not 12!!!!
+    //24 hr clock not 12!!!! from 9 - 5 so 9 to 17 
    
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -30,3 +30,47 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
+
+   
+})
+
+
+
+// another function to track hours and loop over the each time block??
+
+ function trackingHours () {
+       
+    
+    
+    //find the current number of hours? I couldn't find anything on the moment documentation but referenced my tutor
+        var currentTime = moment().hour();
+
+        
+        
+        
+        //function for the-time block class 
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            
+            
+            // console logging the variables
+            console.log( blockHour, currentTime)
+            //else if statement adjusting the styling of the time blocks based on the time of the day
+            if (blockHour < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    trackingHours();
